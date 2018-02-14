e2d650da95db67048b09185ca23737b46267ba76
Z Yin <Z.Yin.MD@gmail.com>
2018-02-14 16:46:03 -0500

    minor change

--- Word.js
+++ Word.js
@@ -6,7 +6,7 @@ var Word = function (word) {
   this.letterArray = [];
   for (let i of word) this.letterArray.push(new Letter(i));
 
-  //the method that returns a string to display the current word status
+  //method that returns a string to display the current word status
   this.display = () => {
     let result = [];
     for (let i of this.letterArray) result.push(i.display());
@@ -16,7 +16,7 @@ var Word = function (word) {
   //array that contains the already tried letters
   this.guessedLetters = [];
 
-  //the guess method takes a letter input and perform the guess
+  //method that takes a letter input and perform the guess
   this.guess = (letter) => {
     if (this.guessedLetters.includes(letter)) return 'dupe'; //first check if this letter has been guessed
     else this.guessedLetters.push(letter); // if not, push letter into guessedLetters
